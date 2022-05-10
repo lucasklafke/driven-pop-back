f069d3111e08c444361aea96e3f935847374f682
Lucas klafke <lucasnkz11@gmail.com>
2022-05-10 20:15:53 -0300

    make it better

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
 import cors from "cors"
 import joi from "joi"
+import dotenv from "dotenv"
+import db from "db.js";
 
 const app = express()
+
+dotenv.config()
 
 PORT = process.env.PORT
 
