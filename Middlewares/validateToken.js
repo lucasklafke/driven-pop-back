6baa9a01b3130d2b158bf897bf1fb49473dee783
Lucas klafke <lucasnkz11@gmail.com>
2022-05-11 22:31:03 -0300

    feat: validateToken

---
+++ Middlewares/validateToken.js
@@ -0,0 +1,17 @@
+import db from "../db.js"
+export default async function validateToken(req,res,next){
+    const { authorization } = req.headers
+    if(!authorization){
+        return res.status(404).send("authorization missed")
+    }
+    const token = authorization.replace("Bearer", "").trim()
+
+    try{
+        const sessions = db.collection("sessions")
+        const session = sessions.findOne({token})
+        if (!session) { return res.status(404).send("token not found")}
+
+    }catch(error){
+        res.send("something went wrong!")
+    }
+}
