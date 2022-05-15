990423db0f4cf1b129b0e3505316f20273b48f39
Lucas klafke <lucasnkz11@gmail.com>
2022-05-14 23:20:16 -0300

    products controller

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -1,11 +1,13 @@
 import db from "../db.js"
 export default async function getProducts(req,res){
-    // const {page} = req.query
+    let {start} = req.query
+    if (!start){
+        return res.send("page not found!")
+    }
     
-
     try{
         const products = db.collection("products")
-        const arrayProducts = await products.find({}).limit(10).toArray()
+        const arrayProducts = await products.find({}).skip(Number(200*(start-1))).limit(200).toArray()
         if(arrayProducts){    
             return res.send(arrayProducts)
         }
