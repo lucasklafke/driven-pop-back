00911264162cf8d1c910a99ad07a331982ad621c
Lucas klafke <lucasnkz11@gmail.com>
2022-05-16 08:37:43 -0300

    before merge

--- Schemas/checkoutInfosSchema.js
+++ Schemas/checkoutInfosSchema.js
@@ -1,9 +1,11 @@
 import joi from "joi"
 
-export default checkoutInfosSchema = joi.object({
+const checkoutInfosSchema = joi.object({
     zip_code: joi.number().required(),
     adress: joi.required,
     adress_number: joi.number().required(),
     adress_complement: joi.string(),
     payment_method: joi.valid("debit","credit card").required()
 })
+
+export default checkoutInfosSchema
